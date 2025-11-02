0ad40e7c32e3668267db9d1255759ddb4bb9afd9
Natalia511 <natiyliza0023@gmail.com>
2025-11-02 14:03:55 -0300

    ajustes
    algunos ajustes en base de datos

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/gametracker", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Conexión exitosa a MongoDB");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Conexión exitosa a MongoDB Atlas");
   } catch (error) {
-    console.error("Error al conectar a MongoDB:", error.message);
+    console.error("❌ Error al conectar a MongoDB:", error.message);
     process.exit(1);
   }
 };
