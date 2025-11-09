1dc0337e143cbffbea5cec4ea44dc9b3d31c2ce8
Natalia511 <natiyliza0023@gmail.com>
2025-11-09 00:48:45 -0300

    pequeño cambio en server.js
    estaba mal el localhost

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ import usuariosRouter from "./src/routes/usuarios.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
 app.use(express.json());
 
 connectDB();
